39363685b819b948cba35a547979284f641897ea
GroellerKarim <karim.jonas.groeller@gmail.com>
2023-05-18 01:41:17 +0200

    added autotag to treesitter config

--- init.lua
+++ init.lua
@@ -298,6 +298,10 @@ require('nvim-treesitter.configs').setup {
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
 
+  autotag = {
+    enable = true,
+  },
+
   highlight = { enable = true },
   indent = { enable = true, disable = { 'python' } },
   incremental_selection = {
